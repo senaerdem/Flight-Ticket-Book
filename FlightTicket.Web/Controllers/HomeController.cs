2d3f901b7e19c15c254305a842aaddf9f14992c9
Sena Nur ERDEM <snnrrdm@gmail.com>
2023-12-21 19:13:46 +0300

    commit2
    home controller

---
+++ FlightTicket.Web/Controllers/HomeController.cs
@@ -0,0 +1,81 @@
+﻿using FlightTicket.Business.Abstract;
+using FlightTicket.Core;
+using FlightTicket.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FlightTicket.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ITripService _tripService;
+        private readonly IMidlineService _midlineService;
+
+        public HomeController(ITripService tripService, IMidlineService midlineService)
+        {
+            _tripService = tripService;
+            _midlineService = midlineService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var midlines = await _midlineService.GetAllAsync();
+            List<string> startingPoints = new List<string>(); 
+            List<string> destinations = new List<string>();
+            foreach (var midline in midlines)
+            {
+                if (!startingPoints.Contains(midline.StartingPoint))
+                {
+                    startingPoints.Add(midline.StartingPoint);
+                }
+                if (!destinations.Contains(midline.Destination))
+                {
+                    destinations.Add(midline.Destination);
+                }
+            };
+            var tripSearchModel = new TripSearchModel()
+            {
+                StartingPoints= startingPoints,
+                Destinations = destinations
+            };
+            return View(tripSearchModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(TripSearchModel SearchModel)
+        {
+            if (User.IsInRole("Operator") || User.IsInRole("Admin"))
+            {
+                return View("_NoAccess");
+            }
+            if (ModelState.IsValid)
+            {
+                SearchModel.Date = Jobs.UpdateDateFormat(SearchModel.Date);
+                return RedirectToAction("TripList","FlightTicket", SearchModel);
+            }
+            else
+            {
+                var midlines = await _midlineService.GetAllAsync();
+                List<string> startingPoints = new List<string>();
+                List<string> destinations = new List<string>();
+                foreach (var midline in midlines)
+                {
+                    if (!startingPoints.Contains(midline.StartingPoint))
+                    {
+                        startingPoints.Add(midline.StartingPoint);
+                    }
+                    if (!destinations.Contains(midline.Destination))
+                    {
+                        destinations.Add(midline.Destination);
+                    }
+                };
+
+                SearchModel.StartingPoints = startingPoints;
+                SearchModel.Destinations = destinations;
+                
+            }
+            return View(SearchModel);
+        }
+
+    }
+}
